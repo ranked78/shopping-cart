437ecbcd63aa8f75966045a10ef80605009382fc
Emman <click2edit@gmail.com>
2024-06-26 23:53:53 +0800

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+const data = [
+    { id: 1, name: 'Item One', price: 12},
+    { id: 2, name: 'Item Two', price: 24},
+    { id: 3, name: 'Item Three', price:55}
+  ];
+  
+  export default data;
+  
---
+++ src/pages/Section.jsx
@@ -0,0 +1,47 @@
+import data from "../data/data"
+
+function Section(){
+    return (<>
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Product ID
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Product Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+            {data.map(item => (
+                <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {item.id}
+                </th>
+                <td className="px-6 py-4">
+                {item.name}
+                </td>
+                <td className="px-6 py-4">
+                ${item.price}
+                </td>
+                <td className="px-6 py-4">
+                  Test Action
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+
+    </>)
+}
+
+export default Section
