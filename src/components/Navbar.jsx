e134b5033b93048067e749bd91dfaf63d781f6f9
Emman <click2edit@gmail.com>
2024-07-01 00:41:54 +0800

    Footer and Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar({ sideNavRef }) {
 
        <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/market-cart-logo-template-design-1276b884c934d54065585fc153cce2eb_screen.jpg?ts=1677860657" className="h-8" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Shopping Cart</span>
         </a>
